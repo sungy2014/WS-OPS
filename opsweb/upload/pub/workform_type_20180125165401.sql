82abacb9c2d5fc0e4b2aff760b861403c87aa60a
root <root@172-17-134-23.(none)>
2018-01-27 22:54:22 +0800

    清理掉多余文件

--- opsweb/upload/pub/workform_type_20180125165401.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `workform_type` (`id`, `name`, `cn_name`, `process_step_id`) VALUES (1, 'publish', '应用发布', '10 -> 20 -> 30 -> 40 -> 50');
-INSERT INTO `workform_type` (`id`, `name`, `cn_name`, `process_step_id`) VALUES (2, 'rollback', '应用回滚', '10 -> 20 -> 30 -> 40 -> 50');
-INSERT INTO `workform_type` (`id`, `name`, `cn_name`, `process_step_id`) VALUES (3, 'server_require', '服务器申请', '10 -> 30 -> 40');
-INSERT INTO `workform_type` (`id`, `name`, `cn_name`, `process_step_id`) VALUES (4, 'permission_require', '权限申请', '10 -> 30 -> 40');
-INSERT INTO `workform_type` (`id`, `name`, `cn_name`, `process_step_id`) VALUES (5, 'app_env_require', '应用环境准备', '10 -> 30 -> 40');
-INSERT INTO `workform_type` (`id`, `name`, `cn_name`, `process_step_id`) VALUES (6, 'sql_exec', 'SQL执行', '10 -> 30 -> 40');
-INSERT INTO `workform_type` (`id`, `name`, `cn_name`, `process_step_id`) VALUES (7, 'others', '其他申请', '10 -> 30 -> 40');
